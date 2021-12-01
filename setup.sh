9a0e4ee63f033dc2c336c6c433ef338137733326
Adam Love <adam.love127@gmail.com>
2021-12-01 14:52:34 +1100

    Configure tmux and update setupsh to handle foodbomb gitconfig

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 # list of files/folders to symlink in homedir
-files=".zshrc .vimrc .tmux.conf .gitconfig .gitignore .config/nvim/init.vim .config/nvim/coc-settings.json"
+files=".zshrc .vimrc .tmux.conf .gitconfig .gitignore .config/nvim/init.vim .config/nvim/coc-settings.json .gitconfig-foodbomb"
 
 ##########
 
