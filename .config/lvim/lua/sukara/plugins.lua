d25bf3aacc78b825518554bf43cf015068b01972
Adam Love <adam.love127@gmail.com>
2023-11-12 21:15:17 +1100

    Add md preview

--- .config/lvim/lua/sukara/plugins.lua
+++ .config/lvim/lua/sukara/plugins.lua
@@ -31,6 +31,14 @@ lvim.plugins = {
 	{
 		"brenoprata10/nvim-highlight-colors",
 	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
 
 	-- {
 	--   "folke/trouble.nvim",
